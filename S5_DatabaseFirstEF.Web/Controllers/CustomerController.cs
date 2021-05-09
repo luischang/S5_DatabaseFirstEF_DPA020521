723ed70b7131ba4213f8ba16dad1234060b039cb
Luis Chang <luis.chang@outlook.com>
2021-05-09 17:39:26 -0500

    CustomerController, CustomerRepository, SweetAlert y Datatable.

---
+++ S5_DatabaseFirstEF.Web/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using S5_DatabaseFirstEF.Web.Models;
+using S5_DatabaseFirstEF.Web.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S5_DatabaseFirstEF.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Listado()
+        {
+            var listadoCliente = await CustomerRepository.ListadoAsync();
+            return PartialView(listadoCliente);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Grabar(
+            int idCliente,
+            string nombres,
+            string apellidos,
+            string pais,
+            string ciudad,
+            string telefono) {
+
+            var customer = new Customer()
+            {
+                FirstName = nombres,
+                LastName = apellidos,
+                City = ciudad,
+                Country = pais,
+                Phone = telefono
+            };
+            var exito = true;
+            if (idCliente == -1)
+                exito = await CustomerRepository.Insertar(customer);
+            else {
+                customer.Id = idCliente;
+                exito = await CustomerRepository.Actualizar(customer);
+            }
+
+            return Json(exito);
+        }
+
+    }
+}
---
+++ S5_DatabaseFirstEF.Web/Repository/CustomerRepository.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore;
+using S5_DatabaseFirstEF.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S5_DatabaseFirstEF.Web.Repository
+{
+    public class CustomerRepository
+    {
+
+        public static IEnumerable<Customer> Listado()
+        {
+            using var data = new SalesContext();
+            return data.Customer.OrderBy(x => x.LastName).ToList();
+        }
+
+        public static async Task<IEnumerable<Customer>> ListadoAsync()
+        {
+            using var data = new SalesContext();
+            return await data.Customer.OrderBy(x => x.LastName).ToListAsync();
+        }
+
+        public static IEnumerable<Customer> ListadoConSP()
+        {
+            using var data = new SalesContext();
+            return data.Customer.FromSqlRaw("SP_SEL_CUSTOMER");
+        }
+
+
+        public static async Task<Customer> Obtener(int id)
+        {
+            using var data = new SalesContext();
+            return await data.Customer.Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public static async Task<bool> Insertar(Customer customer)
+        {
+            bool exito = true;
+
+            try
+            {
+                using var data = new SalesContext();
+                data.Customer.Add(customer);
+                await data.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                exito = false;
+            }
+            return exito;
+        }
+
+        public static async Task<bool> Actualizar(Customer customer)
+        {
+            bool exito = true;
+
+            try
+            {
+                using var data = new SalesContext();
+                var customerNow = await data.Customer.Where(x => x.Id == customer.Id).FirstOrDefaultAsync();
+
+                customerNow.FirstName = customer.FirstName;
+                customerNow.LastName = customer.LastName;
+                customerNow.City = customer.City;
+                customerNow.Country = customer.Country;
+                customerNow.Phone = customer.Phone;
+
+                await data.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                exito = false;
+            }
+            return exito;
+        }
+
+        public static async Task<bool> Eliminar(int id)
+        {
+            bool exito = true;
+
+            try
+            {
+                using var data = new SalesContext();
+                var customerNow = await Obtener(id);
+
+                data.Customer.Remove(customerNow);
+                await data.SaveChangesAsync();
+
+            }
+            catch (Exception)
+            {
+                exito = false;
+            }
+
+            return exito;
+        }
+
+
+    }
+}
